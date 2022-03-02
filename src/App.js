df9386f67e999f8454d5418084a1cfc79685049a
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-03-02 15:30:47 +0100

    nw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react'
 
 const App = () => {
+  const [pagename, setPagename] = useState('Crepeto')
+
+  const titleTime = setTimeout(() => {
+    setPagename(`Management system`)
+  }, 2000)
+  clearTimeout(titleTime)
+
   return (
     <>
       <div className="center">
-        <h1 className="text-justify">Crepeto</h1>
+        <h1 className="text-justify">{pagename}</h1>
       </div>
     </>
   )
