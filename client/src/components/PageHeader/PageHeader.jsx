564e92037f5cfac8ff1463664144f93d4132e4cb
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-12 22:13:34 +0000

    new

--- client/src/components/PageHeader/PageHeader.jsx
+++ client/src/components/PageHeader/PageHeader.jsx
@@ -1,27 +1,38 @@
 import logo from '../../assets/logo/inStock-logo.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './PageHeader.scss'
 
-const PageHeader = ({path}) => {
-    return (
-        <nav className="page-header">
-            <div className="page-header__logo-wrapper">
-                <img className="page-header__logo-img" src={logo} alt="instock logo"/>
-            </div>
-            <div className="page-header__wrapper">
-                <Link 
-                    className={path === "/" ? "page-header__link page-header__link--active" : "page-header__link"} 
-                    to="/">
-                        Warehouses
-                </Link>
-                <Link 
-                    className={path === "/inventory" ? "page-header__link  page-header__link--active" : "page-header__link"} 
-                    to="/inventory">
-                        Inventory
-                </Link>
-            </div>
-        </nav>
-    )
+const PageHeader = ({ path }) => {
+  return (
+    <nav className="page-header">
+      <div className="page-header__logo-wrapper">
+        {/* <img className="page-header__logo-img" src={logo} alt="instock logo"/> */}
+        <h1 className="page-header__logo-img">Crepeto</h1>
+      </div>
+      <div className="page-header__wrapper">
+        <Link
+          className={
+            path === '/'
+              ? 'page-header__link page-header__link--active'
+              : 'page-header__link'
+          }
+          to="/"
+        >
+          Warehouses
+        </Link>
+        <Link
+          className={
+            path === '/inventory'
+              ? 'page-header__link  page-header__link--active'
+              : 'page-header__link'
+          }
+          to="/inventory"
+        >
+          Inventory
+        </Link>
+      </div>
+    </nav>
+  )
 }
 
 export default PageHeader
