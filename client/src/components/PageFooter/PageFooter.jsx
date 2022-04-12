d2f1a99c5121309442ef3620360a81bfbe4437c0
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-12 22:22:31 +0000

    new

--- client/src/components/PageFooter/PageFooter.jsx
+++ client/src/components/PageFooter/PageFooter.jsx
@@ -1,11 +1,11 @@
 import './PageFooter.scss'
 
-const PageFooter = () =>{
-    return (
-        <footer className="page-footer">
-            <p className="page-footer__text">©InStock In. All rights Reserved.</p>
-        </footer>
-    )
+const PageFooter = () => {
+  return (
+    <footer className="page-footer">
+      <p className="page-footer__text">&copy;AYOUB. All rights Reserved.</p>
+    </footer>
+  )
 }
 
 export default PageFooter
