af5da93b0e5e0ccd41e7338a49d4a0a54af45560
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-13 00:08:03 +0000

    Merge branch 'master' of https://github.com/AyoubMaimmadi/Crepeto

--- client/src/components/EmployeeDetails/EmployeeDetail.jsx
+++ client/src/components/EmployeeDetails/EmployeeDetail.jsx
@@ -1,41 +1,43 @@
 import axios from 'axios'
 import { Component } from 'react'
-import './InventoryDetail.scss'
+import './EmployeeDetail.scss.scss'
 import { URL } from '../../utils/api'
 import MainHeader from '../MainHeader/MainHeader'
 
-class InventoryDetail extends Component {
+class EmployeeDetail extends Component {
   state = {
-    inventoryItem: null,
+    employeeItem: null,
   }
   componentDidMount = () => {
     axios
-      .get(`${URL}/inventory/${this.props.match.params.inventoryId}`)
-      .then((res) => this.setState({ inventoryItem: res.data[0] }))
+      .get(`${URL}/employees/${this.props.match.params.employeeId}`)
+      .then((res) => this.setState({ employeeItem: res.data[0] }))
       .catch((err) => console.log(err))
   }
 
   render() {
-    return this.state.inventoryItem === null ? (
+    return this.state.employeeItem === null ? (
       <p>Loading...</p>
     ) : (
       <div className="inventoryDetail">
         <MainHeader
-          headerName={this.state.inventoryItem.itemName}
-          editInventoryId={this.state.inventoryItem.id}
+          headerName={this.state.employeeItem.itemName}
+          editInventoryId={this.state.employeeItem.id}
           navigate={this.props}
         />
         <div className="inventoryDetail__wrapper">
           <div className="inventoryDetail__left">
             <section className="inventoryDetail__left-section">
-              <h4 className="inventoryDetail__left-title">ITEM DESCRIPTION</h4>
+              <h4 className="inventoryDetail__left-title">
+                Employee DESCRIPTION
+              </h4>
               <p className="inventoryDetail__text">
-                {this.state.inventoryItem.description}
+                {this.state.employeeItem.description}
               </p>
 
               <h4 className="inventoryDetail__left-title">CATEGORY</h4>
               <p className="inventoryDetail__text">
-                {this.state.inventoryItem.category}
+                {this.state.employeeItem.category}
               </p>
             </section>
           </div>
@@ -44,22 +46,22 @@ class InventoryDetail extends Component {
               <h4 className="inventoryDetail__right-title">STATUS</h4>
               <h4
                 className={`inventoryDetail__text--status inventoryDetail__text ${
-                  this.state.inventoryItem.status === 'In Stock'
+                  this.state.employeeItem.status === 'In Stock'
                     ? 'inventoryDetail__text--stock'
                     : 'inventoryDetail__text--nostock'
                 }`}
               >
-                {this.state.inventoryItem.status}
+                {this.state.employeeItem.status}
               </h4>
               <h4 className="inventoryDetail__right-title">WAREHOUSE</h4>
               <p className="inventoryDetail__text">
-                {this.state.inventoryItem.warehouseName}
+                {this.state.employeeItem.warehouseName}
               </p>
             </section>
             <section className="inventoryDetail__right-section">
               <h4 className="inventoryDetail__right-title">QUANTITY</h4>
               <p className="inventoryDetail__text">
-                {this.state.inventoryItem.quantity}
+                {this.state.employeeItem.quantity}
               </p>
             </section>
           </div>
@@ -69,4 +71,4 @@ class InventoryDetail extends Component {
   }
 }
 
-export default InventoryDetail
+export default EmployeeDetail
