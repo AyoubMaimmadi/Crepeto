5ee3163f448de5ed1621368f2cbb282096e9fbba
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-13 00:48:03 +0000

    bnew

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,22 @@
-import './App.scss';
-import React, {Component} from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom"
-import WarehousePage from "./pages/WarehousePage/WarehousePage";
-import InventoryPage from './pages/InventoryPage/InventoryPage';
+import './App.scss'
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import WarehousePage from './pages/WarehousePage/WarehousePage'
+import InventoryPage from './pages/InventoryPage/InventoryPage'
 
-
-
-
-
-class App extends Component{
-  
-  render(){
-  return (
-    <div className="app__body">
-      <BrowserRouter>
-      <Switch>
-          
-          <Route path="/inventory" component={InventoryPage}/>
-          <Route path="/" component={WarehousePage}/>
-
-      </Switch>
-      </BrowserRouter>
-    
-    </div>
-  )}
+class App extends Component {
+  render() {
+    return (
+      <div className="app__body">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/inventory" component={InventoryPage} />
+            <Route path="/" component={WarehousePage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
