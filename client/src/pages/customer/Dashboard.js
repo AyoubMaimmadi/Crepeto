ece365b8cee040a61bead30ff41c58d95d76f387
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-14 17:53:57 +0000

    new

--- client/src/pages/customer/Dashboard.js
+++ client/src/pages/customer/Dashboard.js
@@ -1,45 +1,49 @@
-import React, {Component} from "react";
-import PageHeader from "../../components/PageHeader";
+import React, { Component } from 'react'
+import PageHeader from '../../components/PageHeader'
 
-class DashboardPage extends Component{
-    constructor(props){
-        super(props);
-        this.userRole = "owner";
-    }
+class DashboardPage extends Component {
+  constructor(props) {
+    super(props)
+    this.userRole = 'owner'
+  }
 
-
-    render(){
-        return (
-            <div className="admin-content mx-auto">
-                <PageHeader headerText="Welcome to Dashboard"/>
-                <div className="w-75">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-4">
-                                <div className="dash-summary-cell">
-                                    <p><b>Total Products</b></p>
-                                    <p>1200</p>
-                                </div>
-                            </div>
-                            <div className="col-4">
-                                <div className="dash-summary-cell">
-                                    <p><b>Total Invoices</b></p>
-                                    <p>3400</p>
-                                </div>
-                            </div>
-                            <div className="col-4">
-                                <div className="dash-summary-cell">
-                                    <p><b>Total Sell Today</b></p>
-                                    <p>123500</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div className="admin-content mx-auto">
+        <PageHeader headerText="Welcome to Dashboard" />
+        <div className="w-75">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-4">
+                <div className="dash-summary-cell">
+                  <p>
+                    <b>Total Products</b>
+                  </p>
+                  <p>120</p>
                 </div>
-            
+              </div>
+              <div className="col-4">
+                <div className="dash-summary-cell">
+                  <p>
+                    <b>Total Orders</b>
+                  </p>
+                  <p>240</p>
+                </div>
+              </div>
+              <div className="col-4">
+                <div className="dash-summary-cell">
+                  <p>
+                    <b>Total Profit</b>
+                  </p>
+                  <p>MAD 2923.87 </p>
+                </div>
+              </div>
             </div>
-        ) 
-    }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default DashboardPage;
+export default DashboardPage
