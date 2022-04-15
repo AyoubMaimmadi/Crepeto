907b876dd344bbb93053e67b347fed7cceaeed04
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-15 01:13:32 +0000

    Welcome to Dashboard

--- client/src/pages/customer/Dashboard.js
+++ client/src/pages/customer/Dashboard.js
@@ -10,14 +10,16 @@ class DashboardPage extends Component {
   render() {
     return (
       <div className="admin-content mx-auto">
-        <PageHeader headerText="Welcome to Dashboard" />
+        <h1>Crepeto's Management System</h1>
+        <br />
+        <PageHeader headerText="Welcome to the Dashboard" />
         <div className="w-75">
           <div className="container-fluid">
             <div className="row">
               <div className="col-4">
                 <div className="dash-summary-cell">
                   <p>
-                    <b>Total Products</b>
+                    <b>Total Orders</b>
                   </p>
                   <p>120</p>
                 </div>
@@ -25,7 +27,7 @@ class DashboardPage extends Component {
               <div className="col-4">
                 <div className="dash-summary-cell">
                   <p>
-                    <b>Total Orders</b>
+                    <b>Total Products</b>
                   </p>
                   <p>240</p>
                 </div>
