ba4609c240e7f3d1109d9bb5ab7272984a107336
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-01 01:06:28 +0000

    new

--- server/controllers/pool/index.js
+++ server/controllers/pool/index.js
@@ -1,19 +1,20 @@
 const Pool = require('pg').Pool
 
 // link our app to the database
+module.exports = new Pool({
+  user: 'dev',
+  host: 'localhost',
+  database: 'crepeto',
+  password: 'password1',
+  port: 5432,
+})
+
+// Production table in Heroku
 // module.exports = new Pool({
-//   user: 'dev',
-//   host: 'localhost',
-//   database: 'crepeto',
-//   password: 'password1',
+//   user: process.env.USER,
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   connection: process.env.DATABASE_URL,
+//   password: process.env.PASSWORD,
 //   port: 5432,
 // })
-
-module.exports = new Pool({
-  user: 'kfogwdldefhmpj',
-  host: `ec2-3-224-125-117.compute-1.amazonaws.com`,
-  database: 'd1cafg9qdvma7l',
-  password: '15c2e450c43c85f40b86d14e2e80eaa9dec874b45165cc191f698fc0b1c5c794',
-  port: 5432,
-  connection: process.env.DATABASE_URL,
-})
